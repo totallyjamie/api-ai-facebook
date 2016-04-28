0d88e8e917a778fb7a89e8b9c73637eb9c6fc239
totallyjamie <jamie.koay@gmail.com>
2016-04-28 23:20:50 +1000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -42,14 +42,14 @@ function processEvent(event) {
                 if (isDefined(responseData) && isDefined(responseData.facebook)) {
                     try {
                         console.log('Response as formatted message');
-                        sendFBMessage(sender, responseData.facebook);
+                        //sendFBMessage(sender, responseData.facebook);
                         sendGenericMessage(sender);
                     } catch (err) {
                         sendFBMessage(sender, {text: err.message });
                     }
                 } else if (isDefined(responseText)) {
                     console.log('Response as text message');
-                    sendFBMessage(sender, {text: responseText});
+                    //sendFBMessage(sender, {text: responseText});
                     sendGenericMessage(sender);
                 }
 
